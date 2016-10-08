a5b2f5fb22b6ca3f945e64ee6ba91241784e45a2
Comandos <00comandos@gmail.com>
2016-10-08 00:36:03 -0400

    Configurado reproductor

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
 <body>
 	<?php include 'inc/header.php'; ?>
 	
-	
+	<!--REPRODUCTOR-->
+	<section class="reproductor">
+		<iframe id="iframe" width="140%" height="85" allowTransparency="true" border="0" style="border: 0; box-shadow: 0px 0px 0  #fff;" src="http://radioshd.com/radioplayer/#radioucsar"></iframe>
+	</section>
+		
 
   	<?php include 'inc/footer.php'; ?>
   	<?php include 'inc/footer_common.php'; ?>
